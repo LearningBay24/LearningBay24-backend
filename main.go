0e0a64d7e9b6e2cd30cc65929e39b411cfd89763
witcher <witcher@wiredspace.de>
2022-04-15 12:01:53 +0200

    introduce config
    introducing a config file for the database connection in order to avoid
    hard coding credentials.

--- main.go
+++ main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/pelletier/go-toml"
+)
+
+type DB struct {
+	Host     string
+	Port     int16
+	User     string
+	Pass     string
+	Database string
+}
+
+type Config struct {
+	DB DB
+}
+
+var (
+	Conf Config
+)
+
+func initConfig() {
+	data, err := ioutil.ReadFile("./config.toml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = toml.Unmarshal(data, &Conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func setupDbHandle() *sql.DB {
+	dsn := getDataSourceName()
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db
+}
+
+func getDataSourceName() string {
+	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", Conf.DB.User, Conf.DB.Pass, Conf.DB.Host, Conf.DB.Port, Conf.DB.Database)
+}
 
 func main() {
-	fmt.Println("Hello, World!");
+	initConfig()
+	db := setupDbHandle()
 }
