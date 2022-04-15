0525cd6c651f5f4ad74c2161a86bb03f199c4c5b
Schorzel <dominikgeiger99@gmx.de>
2022-04-16 00:51:18 +0200

    Removed hardcoded database connecting
    New functions for courses
    
    - Modified CreateCourse()
    - Added DeleteCourse()
    - Added UpdateCourse()
    - Added DeactivateCourse()
    
    Modified two functions
    
    - Modified CreateCourse()
       Adds the role of creator to a user
    - Modified DeleteCourse()
       Checks if 10 Minutes have passed
       since creation

--- course/course.go
+++ course/course.go
@@ -13,21 +13,14 @@ import (
 )
 
 //CreateCourse takes a name,enrollkey and description and adds a course and forum with that Name in the Database
-func CreateCourse(name, enrollkey string, description null.String, usersid []int) {
+func CreateCourse(db *sql.DB, name, enrollkey string, description null.String, usersid []int) {
 	// TODO: implement check for certificates
 	//Connets to the database
-	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/learningbay24")
-
-	if err != nil {
-		panic(err.Error())
-	} else {
-		fmt.Println("Connected")
-	}
 
 	//Creates a Forum struct (Forum has to be created first because of Foreign Key)
 	f := &models.Forum{Name: name}
 	//Inserts into database
-	err = f.Insert(context.Background(), db, boil.Infer())
+	err := f.Insert(context.Background(), db, boil.Infer())
 	if err != nil {
 		panic(err.Error())
 	}
@@ -43,11 +36,105 @@ func CreateCourse(name, enrollkey string, description null.String, usersid []int
 		shasc := models.UserHasCourse{UserID: usersid[0], CourseID: c.ID, RoleID: 1}
 		err = shasc.Insert(context.Background(), db, boil.Infer())
 		if err != nil {
-			DeleteCourse(c.ID)
+			DeleteCourse(db, c.ID)
 			panic(err.Error())
 		}
 	}
-	defer db.Close()
+
+}
+
+//UpdateCourse takes the ID of a existing course and overwrites the corespoding course and forum with the new Strings(name,enrollkey and description)
+func UpdateCourse(db *sql.DB, id int, name, enrollkey string, description null.String) {
+
+	c, err := models.FindCourse(context.Background(), db, id)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	c.EnrollKey = enrollkey
+	c.Description = description
+	c.Name = name
+
+	cupdate, err := c.Update(context.Background(), db, boil.Infer())
+	fmt.Println(cupdate)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	f, err := models.FindForum(context.Background(), db, id)
+	if err != nil {
+		panic(err.Error())
+	}
+	f.Name = name
+
+	fupdate, err := f.Update(context.Background(), db, boil.Infer())
+	fmt.Println(fupdate)
+	if err != nil {
+		panic(err.Error())
+	}
+
+}
+
+//DeleteCourse takes a ID and deletes the course and the forum associated with it
+func DeleteCourse(db *sql.DB, id int) {
+
+	c, err := models.FindCourse(context.Background(), db, id)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	//Checks if more than 10 Minutes have passed wont delete if thats the case
+	curTime := time.Now()
+	diff := curTime.Sub(c.CreatedAt.Time)
+	if diff.Minutes() < 10 {
+		fmt.Println("Only ", diff.Minutes(), "Have passed")
+		cdel, err := c.Delete(context.Background(), db, true)
+		fmt.Println(cdel)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		f, err := models.FindForum(context.Background(), db, id)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		fdel, err := f.Delete(context.Background(), db, true)
+		fmt.Println(fdel)
+		if err != nil {
+			panic(err.Error())
+		}
+	} else {
+		fmt.Println("More than 10 Minutes have passed")
+	}
+
+}
+
+//DeactivateCourse takes a ID and deactivates the course and the forum associated with it
+func DeactivateCourse(db *sql.DB, id int) {
+
+	c, err := models.FindCourse(context.Background(), db, id)
+	if err != nil {
+		panic(err.Error())
+	}
+	c.DeletedAt = null.NewTime(time.Now(), true)
+	cupdate, err := c.Update(context.Background(), db, boil.Infer())
+	fmt.Println(cupdate)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	f, err := models.FindForum(context.Background(), db, id)
+	if err != nil {
+		panic(err.Error())
+	}
+	f.DeletedAt = null.NewTime(time.Now(), true)
+	fudate, err := f.Update(context.Background(), db, boil.Infer())
+	fmt.Println(fudate)
+	if err != nil {
+		panic(err.Error())
+	}
+
 }
 
 //UpdateCourse takes the ID of a existing course and overwrites the corespoding course and forum with the new Strings(name,enrollkey and description)
