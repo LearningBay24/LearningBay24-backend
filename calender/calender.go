33b007378176eef78b88b4ffa3a5f39ae56298b6
Adrian <pasaguna123@gmail.com>
2022-04-18 12:03:46 +0200

    add new calender package

---
+++ calender/calender.go
@@ -0,0 +1,38 @@
+package calender
+
+import "database/sql"
+
+func AppointmentInCalender(db *sql.DB) {
+
+	/*TODO: adjust the code onto the db-object
+
+	// <given variables>___________________________________________________________________________________
+	var appointName = "Terminname"
+	var appointDate = "Termindatum"
+	var appointBegin = "UhrzeitTerminbeginn"
+	var appointEnd = "UhrzeitTerminende"
+	repeats := false
+	var wiederholAbstand = 0 // wenn nicht, dann = 0; 1 = wöchentlich, 2 = monatlich, 3 = jährlich
+	var wiederholEnde = 0    // wenn nicht, dann = 0 bzw. null
+	var zusatzInfo = "Zusatzinfo"
+
+	var courseMembers []string // In Wirklichkeit eine gegebene Abhängigkeit zum dazuehörigen Kurs
+	// </given variables>__________________________________________________________________________________
+
+	// new Appointment appointment{appointName, appointDate, ...}
+
+	for i := 0; i < len(courseMembers); i++ {
+		// check, whether it is an repeating appointment
+		if repeats {
+			// Im gegebenen Abstand den jeweiligen Kalender durchgehen, Termin wie bei else einfügen
+			// for ... {
+			//		courseMembers[i]->calender.insert(appointment)
+			// }
+		} else {
+			// courseMembers[i]->calender.insert(appointment)
+		}
+	}
+
+
+	*/
+}
