96ffc2a5de41063b08d6279fbb24492d82a579dd
witcher <witcher@wiredspace.de>
2022-04-18 15:19:14 +0200

    move db initialization into config package

---
+++ config/db.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func SetupDbHandle() *sql.DB {
+	dsn := getDataSourceName()
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db
+}
+
+func getDataSourceName() string {
+	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", Conf.DB.User, Conf.DB.Pass, Conf.DB.Host, Conf.DB.Port, Conf.DB.Database)
+}
--- main.go
+++ main.go
@@ -1,29 +1,11 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	_ "github.com/go-sql-driver/mysql"
-	"log"
-
 	"learningbay24.de/backend/config"
 )
 
-func setupDbHandle() *sql.DB {
-	dsn := getDataSourceName()
-	db, err := sql.Open("mysql", dsn)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return db
-}
-
-func getDataSourceName() string {
-	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", config.Conf.DB.User, config.Conf.DB.Pass, config.Conf.DB.Host, config.Conf.DB.Port, config.Conf.DB.Database)
-}
-
 func main() {
 	config.InitConfig()
-	db := setupDbHandle()
+	// TODO: do something with the db handle
+	_ = config.SetupDbHandle()
 }
