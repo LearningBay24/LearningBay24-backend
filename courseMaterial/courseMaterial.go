684d476ac211e1a16608c481de8b76bbae07e216
Karparetzu <juli.a98@live.de>
2022-04-21 00:03:30 +0200

    Update courseMaterial.go

--- courseMaterial/courseMaterial.go
+++ courseMaterial/courseMaterial.go
@@ -5,18 +5,19 @@ import (
 	"database/sql"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/volatiletech/null/v8"
 )
 
-func CreateMaterial(db *sql.DB, name, enrollkey string, description null.String, usersid []int) error {
+func CreateMaterial(db *sql.DB, name, url string, usersid int, courseID int) error {
 
-	//Begin transaction with database handle
+	// Begin transaction with database handle
 	tx, err := db.BeginTx(context.Background(), nil)
 	if err != nil {
 		return err
 	}
 
-	//Commit transaction into database
+	// TODO: local boolean
+
+	// Commit transaction into database
 	tx.Commit()
 	return nil
 }
