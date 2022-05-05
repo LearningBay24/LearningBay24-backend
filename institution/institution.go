96d980fcd48cff5f05577dc105e83422a0b46712
Adrian <pasaguna123@gmail.com>
2022-05-05 13:27:28 +0200

    add branch

---
+++ institution/institution.go
@@ -0,0 +1,14 @@
+package institution
+
+/*
+import (
+	"context"
+	"database/sql"
+	"time"
+
+	"github.com/volatiletech/null/v8"
+	"github.com/volatiletech/sqlboiler/v4/boil"
+	"learningbay24.de/backend/models"
+)*/
+
+func 
