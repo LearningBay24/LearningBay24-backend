d002d1d3411f9f7542c29042a2ed39187b5b5161
Karparetzu <juli.a98@live.de>
2022-05-05 20:51:06 +0200

    Expanded Insert, added Update and Delete

--- courseMaterial/courseMaterial.go
+++ courseMaterial/courseMaterial.go
@@ -9,7 +9,7 @@ import (
 	"learningbay24.de/backend/models"
 )
 
-func CreateMaterial(db *sql.DB, name, uri string, uploaderid, courseID int, local int8) error {
+func CreateMaterial(db *sql.DB, name string, uri string, uploaderid, courseID int, local int8) error {
 
 	// Begin transaction with database handle
 	tx, err := db.BeginTx(context.Background(), nil)
@@ -21,15 +21,60 @@ func CreateMaterial(db *sql.DB, name, uri string, uploaderid, courseID int, loca
 	cm := &models.File{Name: name, URI: uri, UploaderID: uploaderid, Local: local}
 
 	err = cm.Insert(context.Background(), db, boil.Infer())
-	/*
-		if err != nil {
-			tx.Rollback()
-			return err
-		} else {
-
-		}
-	*/
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
 	// Commit transaction into database
+	tx.Commit()
+	return nil
+}
+
+// Takes id and updates name and uri
+func UpdateMaterial(db *sql.DB, id int, name, uri string) error {
+	tx, err := db.BeginTx(context.Background(), nil)
+	if err != nil {
+		return err
+	}
+
+	c, err := models.FindFile(context.Background(), db, id)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	c.Name = name
+	c.URI = uri
+
+	_, err = c.Update(context.Background(), db, boil.Infer())
+
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	tx.Commit()
+	return nil
+}
+
+func DeleteFile(db *sql.DB, id int) error {
+	tx, err := db.BeginTx(context.Background(), nil)
+	if err != nil {
+		return err
+	}
+
+	c, err := models.FindFile(context.Background(), db, id)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	_, err = c.Delete(context.Background(), db, true)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
 	tx.Commit()
 	return nil
 }
