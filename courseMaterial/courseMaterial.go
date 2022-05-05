8446f349373d610f9a20da38b2bb2c62a72c2ee3
Karparetzu <juli.a98@live.de>
2022-05-05 22:50:32 +0200

    Updated courseMaterial, added devComment-file (thought-archivation)

--- courseMaterial/courseMaterial.go
+++ courseMaterial/courseMaterial.go
@@ -3,35 +3,67 @@ package coursematerial
 import (
 	"context"
 	"database/sql"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/volatiletech/null/v8"
 	"github.com/volatiletech/sqlboiler/v4/boil"
 	"learningbay24.de/backend/models"
 )
 
+// CreateMaterial takes a name, URI, associated uploader-id, course, id and indicator if file is local or remote
+// Created struct gets inserted into database
 func CreateMaterial(db *sql.DB, name string, uri string, uploaderid, courseID int, local int8) error {
 
-	// Begin transaction with database handle
 	tx, err := db.BeginTx(context.Background(), nil)
 	if err != nil {
 		return err
 	}
 
-	// Creates File (courseMaterial) struct
 	cm := &models.File{Name: name, URI: uri, UploaderID: uploaderid, Local: local}
 
 	err = cm.Insert(context.Background(), db, boil.Infer())
 	if err != nil {
 		tx.Rollback()
 		return err
+	} else {
+
 	}
-	// Commit transaction into database
 	tx.Commit()
 	return nil
 }
 
-// Takes id and updates name and uri
-func UpdateMaterial(db *sql.DB, id int, name, uri string) error {
+// DeactivateMaterial takes an ID and deactivates the chosen material
+// Sets deactivation-timer and updates database
+func DeactivateMaterial(db *sql.DB, id int) error {
+	tx, err := db.BeginTx(context.Background(), nil)
+	if err != nil {
+		return err
+	}
+
+	cm, err := models.FindFile(context.Background(), db, id)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	cm.DeletedAt = null.NewTime(time.Now(), true)
+	_, err = cm.Update(context.Background(), db, boil.Infer())
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	tx.Commit()
+	return nil
+}
+
+/*
+ Switches out file if creator wants to change it
+ Takes id and updates name and uri
+*/
+/*
+ func UpdateMaterial(db *sql.DB, id int, name, uri string) error {
 	tx, err := db.BeginTx(context.Background(), nil)
 	if err != nil {
 		return err
@@ -56,25 +88,4 @@ func UpdateMaterial(db *sql.DB, id int, name, uri string) error {
 	tx.Commit()
 	return nil
 }
-
-func DeleteFile(db *sql.DB, id int) error {
-	tx, err := db.BeginTx(context.Background(), nil)
-	if err != nil {
-		return err
-	}
-
-	c, err := models.FindFile(context.Background(), db, id)
-	if err != nil {
-		tx.Rollback()
-		return err
-	}
-
-	_, err = c.Delete(context.Background(), db, true)
-	if err != nil {
-		tx.Rollback()
-		return err
-	}
-
-	tx.Commit()
-	return nil
-}
+*/
