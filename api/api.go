5ba07277a31b143217bc666a20fb4f7804418f22
Schorzel <48302525+Schorzel@users.noreply.github.com>
2022-05-06 17:28:09 +0200

    Extend API interface for courses (#28)
    * Added 3 new Functions
    
    - Added GetCourse()
    - Added GetUserCourses()
    - Added DeleteUserFromCourse()
    
    * Added function to enroll a user in course
    
    - Added enrollkey to CreateCourse()
    - Added the function to show users in a course
    - Removed raw SQL statement
    
    * Update course.go
    
    - Added more return parameter for certain functions
    - Fixed database transactions
    - Added error handling to Commit() and Rollback() in functions
    
    * New Api functionality for course backend
    
    - Changed Return Values
    - Merged DeleteCourse() and DeactivateCourse()
    
    * Fixed some issues
    
    - Added missing returns
    - Softdelete userhascourse instead harddelete
    
    Co-authored-by: Dominik2497 <50696605+Dominik2497@users.noreply.github.com>

---
+++ api/api.go
@@ -0,0 +1,213 @@
+package api
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"learningbay24.de/backend/course"
+	"learningbay24.de/backend/models"
+)
+
+type PublicController struct {
+	Database *sql.DB
+}
+
+func (f *PublicController) GetCourseById(c *gin.Context) {
+	//Get given ID from the Context
+	//Convert data type from str to int to use ist as param
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+	//Fetch Data from Database with Backend function
+	course, err := course.GetCourse(f.Database, id)
+	if err != nil {
+		log.Println(err)
+		c.IndentedJSON(http.StatusBadRequest, err.Error())
+		return
+	}
+	//Return Status and Data in JSON-Format
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.IndentedJSON(http.StatusOK, course)
+	log.Println("course ", course)
+	log.Println(err)
+}
+
+func (f *PublicController) DeleteUserFromCourse(c *gin.Context) {
+	//Get given ID from the Context
+	//Convert data type from str to int to use ist as param
+	user_id, err := strconv.Atoi(c.Param("user_id"))
+	if err != nil {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+	//Fetch Data from Database with Backend function
+	err = course.DeleteUserFromCourse(f.Database, id, user_id)
+	if err != nil {
+		log.Println(err)
+		c.IndentedJSON(http.StatusBadRequest, err.Error())
+		return
+	}
+	//Return Status and Data in JSON-Format
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Status(http.StatusNoContent)
+
+}
+
+func (f *PublicController) GetUsersInCourse(c *gin.Context) {
+
+	//Get given ID from the Context
+	//Convert data type from str to int to use ist as param
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+	//Fetch Data from Database with Backend function
+	users, err := course.GetUsersInCourse(f.Database, id)
+	if err != nil {
+		log.Println(err)
+		c.IndentedJSON(http.StatusBadRequest, err.Error())
+		return
+	}
+	//Return Status and Data in JSON-Format
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.IndentedJSON(http.StatusOK, users)
+	log.Println("course ", users)
+}
+
+func (f *PublicController) GetUserCourses(c *gin.Context) {
+
+	//Get given ID from the Context
+	//Convert data type from str to int to use ist as param
+	user_id, err := strconv.Atoi(c.Param("user_id"))
+	if err != nil {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+	//Fetch Data from Database with Backend function
+	courses, err := course.GetCoursesFromUser(f.Database, user_id)
+	if err != nil {
+		log.Println(err)
+		c.IndentedJSON(http.StatusBadRequest, err.Error())
+		return
+	}
+	//Return Status and Data in JSON-Format
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.IndentedJSON(http.StatusOK, courses)
+	log.Println("course ", courses)
+
+}
+
+func (f *PublicController) DeleteCourse(c *gin.Context) {
+
+	//Get given ID from the Context
+	//Convert data type from str to int to use ist as param
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+	//Deactivate Data from Database with Backend function
+	course, err := course.DeleteCourse(f.Database, id)
+	//Return Status and Data in JSON-Format
+	if err != nil {
+		log.Println(err)
+		c.IndentedJSON(http.StatusBadRequest, err.Error())
+		return
+	}
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.IndentedJSON(http.StatusOK, course)
+	log.Println("course ", course)
+}
+
+func (f *PublicController) CreateCourse(c *gin.Context) {
+
+	var newCourse models.Course
+
+	if err := c.BindJSON(&newCourse); err != nil {
+		if err != nil {
+			log.Println(err)
+			c.IndentedJSON(http.StatusBadRequest, err.Error())
+			return
+		}
+	}
+	id, err := course.CreateCourse(f.Database, newCourse.Name, newCourse.Description, newCourse.EnrollKey, newCourse.ID)
+	if err != nil {
+		log.Println(err)
+		c.IndentedJSON(http.StatusBadRequest, err.Error())
+		return
+	}
+	newCourse.ID = id
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.IndentedJSON(http.StatusOK, newCourse)
+}
+
+func (f *PublicController) EnrollUser(c *gin.Context) {
+
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+	log.Println(err.Error())
+	user_id, err := strconv.Atoi(c.Param("user_id"))
+	if err != nil {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+	var newCourse models.Course
+	if err := c.BindJSON(&newCourse); err != nil {
+		if err != nil {
+			log.Println(err)
+			c.IndentedJSON(http.StatusBadRequest, err.Error())
+			return
+		}
+	}
+
+	user, err := course.EnrollUser(f.Database, user_id, id, newCourse.EnrollKey)
+
+	if err != nil {
+		log.Println(err)
+		c.IndentedJSON(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	c.Header("Access-Control-Allow-Origin", "*")
+	log.Println("user", user)
+	c.IndentedJSON(http.StatusOK, newCourse)
+}
+
+func (f *PublicController) UpdateCourseById(c *gin.Context) {
+
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+	var newCourse models.Course
+	if err := c.BindJSON(&newCourse); err != nil {
+		log.Println(err)
+		c.IndentedJSON(http.StatusBadRequest, err.Error())
+		return
+	}
+	course, err := course.UpdateCourse(f.Database, id, newCourse.Name, newCourse.Description, newCourse.EnrollKey)
+	if err != nil {
+		log.Println(err)
+		c.IndentedJSON(http.StatusBadRequest, err.Error())
+		return
+	}
+	newCourse.ID = id
+	log.Println("course", course)
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.IndentedJSON(http.StatusOK, newCourse)
+}
--- course/course.go
+++ course/course.go
@@ -4,170 +4,328 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/volatiletech/null/v8"
 	"github.com/volatiletech/sqlboiler/v4/boil"
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 	"learningbay24.de/backend/models"
 )
 
+// GetCourse takes a ID and returns a struct of the course with this ID
+func GetCourse(db *sql.DB, id int) (*models.Course, error) {
+
+	c, err := models.FindCourse(context.Background(), db, id)
+	if err != nil {
+		return nil, err
+	}
+	return c, nil
+}
+
 // CreateCourse takes a name,enrollkey and description and adds a course and forum with that Name in the Database while userid is an array of IDs that is used to assign the role of the creator
 // and the roles for tutor
-func CreateCourse(db *sql.DB, name, enrollkey string, description null.String, usersid []int) error {
+func CreateCourse(db *sql.DB, name string, description null.String, enrollkey string, usersid int) (int, error) {
 	// TODO: implement check for certificates
 
 	// Begins the transaction
 	tx, err := db.BeginTx(context.Background(), nil)
 	if err != nil {
-		return err
+		return 0, err
 	}
 	// Creates a Forum struct (Forum has to be created first because of Foreign Key)
 	f := &models.Forum{Name: name}
 	// Inserts into database
-	err = f.Insert(context.Background(), db, boil.Infer())
+	err = f.Insert(context.Background(), tx, boil.Infer())
 	if err != nil {
-		tx.Rollback()
-		return err
+		if e := tx.Rollback(); e != nil {
+			return 0, fmt.Errorf("fatal: unable to rollback transaction on error: %s; %s", err.Error(), e.Error())
+		}
+
+		return 0, err
 	}
 
 	// Creates a Course struct
-	c := &models.Course{Name: name, ForumID: f.ID, Description: description}
+	c := &models.Course{Name: name, Description: description, EnrollKey: enrollkey, ForumID: f.ID}
 	// Inserts into database
-	err = c.Insert(context.Background(), db, boil.Infer())
+	err = c.Insert(context.Background(), tx, boil.Infer())
 	if err != nil {
-		tx.Rollback()
-		return err
+		if e := tx.Rollback(); e != nil {
+			return 0, fmt.Errorf("fatal: unable to rollback transaction on error: %s; %s", err.Error(), e.Error())
+		}
+
+		return 0, err
 	} else {
 		// TODO: Implement roles assigment for tutors
 		// Gives the user with the ID in the 0 place in the array the role of the creator
-		shasc := models.UserHasCourse{UserID: usersid[0], CourseID: c.ID, RoleID: 1}
-		err = shasc.Insert(context.Background(), db, boil.Infer())
+		shasc := models.UserHasCourse{UserID: usersid, CourseID: c.ID, RoleID: 1}
+		err = shasc.Insert(context.Background(), tx, boil.Infer())
 		if err != nil {
-			tx.Rollback()
-			return err
+			if e := tx.Rollback(); e != nil {
+				return 0, fmt.Errorf("fatal: unable to rollback transaction on error: %s; %s", err.Error(), e.Error())
+			}
+
+			return 0, err
 		}
 	}
-	tx.Commit()
-	return nil
+	if e := tx.Commit(); e != nil {
+		return 0, fmt.Errorf("fatal: unable to rollback transaction on error: %s; %s", err.Error(), e.Error())
+	}
+	return c.ID, nil
 }
 
 // UpdateCourse takes the ID of a existing course and the already existing fields for name,enrollkey and description and overwrites the corespoding course and forum with the new Strings(name,enrollkey and description)
-func UpdateCourse(db *sql.DB, id int, name, enrollkey string, description null.String) error {
+func UpdateCourse(db *sql.DB, id int, name string, description null.String, enrollkey string) (int, error) {
 	tx, err := db.BeginTx(context.Background(), nil)
 	if err != nil {
-		return err
+		return 0, err
 	}
 
-	c, err := models.FindCourse(context.Background(), db, id)
+	c, err := models.FindCourse(context.Background(), tx, id)
 	if err != nil {
-		tx.Rollback()
-		return err
-	}
+		if e := tx.Rollback(); e != nil {
+			return 0, fmt.Errorf("fatal: unable to rollback transaction on error: %s; %s", err.Error(), e.Error())
+		}
 
+		return 0, err
+	}
 	c.EnrollKey = enrollkey
 	c.Description = description
 	c.Name = name
 
-	_, err = c.Update(context.Background(), db, boil.Infer())
+	_, err = c.Update(context.Background(), tx, boil.Infer())
 
 	if err != nil {
-		tx.Rollback()
-		return err
+		if e := tx.Rollback(); e != nil {
+			return 0, fmt.Errorf("fatal: unable to rollback transaction on error: %s; %s", err.Error(), e.Error())
+		}
+
+		return 0, err
 	}
 
-	f, err := models.FindForum(context.Background(), db, id)
+	f, err := models.FindForum(context.Background(), tx, id)
 	if err != nil {
-		tx.Rollback()
-		return err
+		if e := tx.Rollback(); e != nil {
+			return 0, fmt.Errorf("fatal: unable to rollback transaction on error: %s; %s", err.Error(), e.Error())
+		}
+
+		return 0, err
 	}
 	f.Name = name
 
-	_, err = f.Update(context.Background(), db, boil.Infer())
+	_, err = f.Update(context.Background(), tx, boil.Infer())
 
 	if err != nil {
-		tx.Rollback()
-		return err
+		if e := tx.Rollback(); e != nil {
+			return 0, fmt.Errorf("fatal: unable to rollback transaction on error: %s; %s", err.Error(), e.Error())
+		}
+
+		return 0, err
 	}
-	tx.Commit()
-	return nil
+	if e := tx.Commit(); e != nil {
+		return 0, fmt.Errorf("fatal: unable to commit transaction on error: %s; %s", err, e)
+	}
+	return c.ID, nil
 }
 
 // DeleteCourse takes a ID and deletes the course and the forum associated with it
-func DeleteCourse(db *sql.DB, id int) error {
+func DeleteCourse(db *sql.DB, id int) (int, error) {
 	tx, err := db.BeginTx(context.Background(), nil)
 	if err != nil {
-		return err
+		return 0, err
 	}
-
-	c, err := models.FindCourse(context.Background(), db, id)
+	// Check if there are more users in the course besides the creator
+	userhascourse, err := models.UserHasCourses(models.UserHasCourseWhere.CourseID.EQ(id)).Count(context.Background(), db)
 	if err != nil {
-		tx.Rollback()
-		return err
+		return 0, err
+	}
+	if userhascourse > 1 {
+		return 0, errors.New("there are still people enrolled in the course besides the creator")
+	}
+	// Get the creator of the course
+	userinc, err := GetUsersInCourse(db, id)
+	if err != nil {
+		return 0, err
+	}
+	// Its just creator in the course so delete him
+	err = DeleteUserFromCourse(db, userinc[0].ID, id)
+	if err != nil {
+		return 0, err
+	}
+	c, err := models.FindCourse(context.Background(), tx, id)
+	if err != nil {
+		if e := tx.Rollback(); e != nil {
+			return 0, fmt.Errorf("fatal: unable to rollback transaction on error: %s; %s", err.Error(), e.Error())
+		}
+
+		return 0, err
+	}
+	f, err := models.FindForum(context.Background(), tx, id)
+	if err != nil {
+		if e := tx.Rollback(); e != nil {
+			return 0, fmt.Errorf("fatal: unable to rollback transaction on error: %s; %s", err.Error(), e.Error())
+		}
+
+		return 0, err
 	}
 
-	// Checks if more than 10 Minutes have passed wont delete if thats the case
+	// Checks if more than 10 Minutes have passed will softdelete if thats the case
 	curTime := time.Now()
 	diff := curTime.Sub(c.CreatedAt.Time)
 	if diff.Minutes() < 10 {
-
-		_, err = c.Delete(context.Background(), db, true)
+		_, err = c.Delete(context.Background(), tx, false)
 		if err != nil {
-			tx.Rollback()
-			return err
-		}
+			if e := tx.Rollback(); e != nil {
+				return 0, fmt.Errorf("fatal: unable to rollback transaction on error: %s; %s", err.Error(), e.Error())
+			}
 
-		f, err := models.FindForum(context.Background(), db, id)
-		if err != nil {
-			tx.Rollback()
-			return err
+			return 0, err
 		}
+		_, err = f.Delete(context.Background(), tx, false)
+		if err != nil {
+			if e := tx.Rollback(); e != nil {
+				return 0, fmt.Errorf("fatal: unable to rollback transaction on error: %s; %s", err.Error(), e.Error())
+			}
 
-		_, err = f.Delete(context.Background(), db, true)
-		if err != nil {
-			tx.Rollback()
-			return err
+			return 0, err
 		}
-	} else {
-		return errors.New("More than 10 Minutes have passed")
+		if e := tx.Commit(); e != nil {
+			return 0, fmt.Errorf("fatal: unable to commit transaction on error: %s; %s", err, e)
+		}
+		return c.ID, nil
+
 	}
-	tx.Commit()
-	return nil
+
+	_, err = c.Delete(context.Background(), tx, true)
+	if err != nil {
+		if e := tx.Rollback(); e != nil {
+			return 0, fmt.Errorf("fatal: unable to rollback transaction on error: %s; %s", err.Error(), e.Error())
+		}
+		return 0, err
+	}
+
+	_, err = f.Delete(context.Background(), tx, true)
+	if err != nil {
+		if e := tx.Rollback(); e != nil {
+			return 0, fmt.Errorf("fatal: unable to rollback transaction on error: %s; %s", err.Error(), e.Error())
+		}
+
+		return 0, err
+	}
+	if e := tx.Commit(); e != nil {
+		return 0, fmt.Errorf("fatal: unable to commit transaction on error: %s; %s", err, e)
+	}
+	return c.ID, nil
 }
 
-// DeactivateCourse takes a ID and deactivates the course and the forum associated with it
-func DeactivateCourse(db *sql.DB, id int) error {
+// GetCoursesFromUser takes the ID of a User and returns a slice of Courses in which he is enrolled
+func GetCoursesFromUser(db *sql.DB, uid int) (models.CourseSlice, error) {
+
+	courses, err := models.Courses(
+		qm.From(models.TableNames.UserHasCourse),
+		qm.Where("user_has_course.user_id=?", uid),
+		qm.And("user_has_course.course_id = course.id"),
+	).All(context.Background(), db)
+	if err != nil {
+		return nil, err
+	}
+
+	return courses, nil
+}
+
+// GetUserCourses takes the ID of a Course and returns a slice of Users which are enrolled in it
+func GetUsersInCourse(db *sql.DB, cid int) (models.UserSlice, error) {
+
+	users, err := models.Users(
+		qm.From(models.TableNames.UserHasCourse),
+		qm.Where("user_has_course.course_id=?", cid),
+		qm.And("user_has_course.user_id = user.id"),
+	).All(context.Background(), db)
+	if err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
+// DeleteUserFromCourse takes a UserID and a CourseID and deletes the corresponding entry in the table "user_has_course"
+func DeleteUserFromCourse(db *sql.DB, uid int, cid int) error {
 
 	tx, err := db.BeginTx(context.Background(), nil)
 	if err != nil {
+
 		return err
 	}
 
-	c, err := models.FindCourse(context.Background(), db, id)
+	userhascourse, err := models.FindUserHasCourse(context.Background(), tx, uid, cid)
 	if err != nil {
-		tx.Rollback()
-		return err
-	}
-	c.DeletedAt = null.NewTime(time.Now(), true)
-	_, err = c.Update(context.Background(), db, boil.Infer())
+		if e := tx.Rollback(); e != nil {
+			return fmt.Errorf("fatal: unable to rollback transaction on error: %s; %s", err, e)
+		}
 
-	if err != nil {
-		tx.Rollback()
 		return err
 	}
 
-	f, err := models.FindForum(context.Background(), db, id)
+	_, err = userhascourse.Delete(context.Background(), tx, false)
 	if err != nil {
-		tx.Rollback()
-		return err
-	}
-	f.DeletedAt = null.NewTime(time.Now(), true)
-	_, err = f.Update(context.Background(), db, boil.Infer())
+		if e := tx.Rollback(); e != nil {
+			return fmt.Errorf("fatal: unable to rollback transaction on error: %s; %s", err, e)
+		}
 
-	if err != nil {
-		tx.Rollback()
 		return err
 	}
-	tx.Commit()
+
+	if e := tx.Commit(); e != nil {
+		return fmt.Errorf("fatal: unable to commit transaction on error: %s; %s", err, e)
+	}
 	return nil
+}
+
+// EnrollUser takes a UserID, CourseID and Enrollkey and adds the User to the course if the enrollkey is correct
+func EnrollUser(db *sql.DB, uid int, cid int, enrollkey string) (*models.User, error) {
+
+	tx, err := db.BeginTx(context.Background(), nil)
+	if err != nil {
+
+		return nil, err
+	}
+
+	c, err := models.FindCourse(context.Background(), tx, cid)
+	if err != nil {
+		if e := tx.Rollback(); e != nil {
+			return nil, fmt.Errorf("fatal: unable to rollback transaction on error: %s; %s", err, e)
+		}
+
+		return nil, err
+	}
+	if c.EnrollKey != enrollkey {
+		if e := tx.Rollback(); e != nil {
+			return nil, fmt.Errorf("fatal: unable to rollback transaction on error: %s; %s", err, e)
+		}
+		return nil, errors.New("wrong Enrollkey")
+
+	}
+	userhascourse := models.UserHasCourse{UserID: uid, CourseID: cid, RoleID: 3}
+	err = userhascourse.Insert(context.Background(), tx, boil.Infer())
+	if err != nil {
+		if e := tx.Rollback(); e != nil {
+			return nil, fmt.Errorf("fatal: unable to rollback transaction on error: %s; %s", err, e)
+		}
+
+		return nil, err
+	}
+	u, err := models.FindUser(context.Background(), tx, uid)
+	if err != nil {
+		if e := tx.Rollback(); e != nil {
+			return nil, fmt.Errorf("fatal: unable to rollback transaction on error: %s; %s", err, e)
+		}
+
+		return nil, err
+	}
+	if e := tx.Commit(); e != nil {
+		return nil, fmt.Errorf("fatal: unable to commit transaction on error: %s; %s", err, e)
+	}
+	return u, nil
+
 }
