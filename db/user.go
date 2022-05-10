673e8d87054ca54b69edc37c6a97a09b72c27e3b
witcher <witcher@wiredspace.de>
2022-05-10 19:14:19 +0200

    user: remove redundant input validation

--- db/user.go
+++ db/user.go
@@ -13,45 +13,11 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-func validateUserFields(user *models.User) error {
-	if user.Title.Valid && len(user.Title.String) > 64 {
-		return errors.New("Field \"title\" is too long, only 64 characters allowed")
-	}
-
-	if len(user.Firstname) > 32 {
-		return errors.New("Field \"firstname\" is too long, only 32 characters allowed")
-	}
-
-	if len(user.Surname) > 32 {
-		return errors.New("Field \"surname\" is too long, only 32 characters allowed")
-	}
-
-	if len(user.Email) > 256 {
-		return errors.New("Field \"email\" is too long, only 256 characters allowed")
-	}
-
-	if user.PhoneNumber.Valid && len(user.PhoneNumber.String) > 45 {
-		return errors.New("Field \"phone_number\" is too long, only 45 characters allowed")
-	}
-
-	if user.Residence.Valid && len(user.Residence.String) > 256 {
-		return errors.New("Field \"residence\" is too long, only 256 characters allowed")
-	}
-
-	if user.Biography.Valid && len(user.Biography.String) > 512 {
-		return errors.New("Field \"biography\" is too long, only 512 characters allowed")
-	}
-
-	return nil
-}
-
 // Create a user with a given password as []byte.
 // the cleartext password received will be hashed in this function.
 func CreateUser(db *sql.DB, user models.User) (int, error) {
-	err := validateUserFields(&user)
-	if err != nil {
-		return 0, err
-	}
+	// input validation is done on the database level
+	// error is being thrown when something cannot be inserted
 
 	password, err := bcrypt.GenerateFromPassword(user.Password, bcrypt.DefaultCost)
 	if err != nil {
