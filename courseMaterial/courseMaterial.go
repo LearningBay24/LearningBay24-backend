49cc70ef91a24703cd46ae2c00eda9108f889882
Karparetzu <juli.a98@live.de>
2022-05-12 22:32:58 +0200

    Update courseMaterial

--- courseMaterial/courseMaterial.go
+++ courseMaterial/courseMaterial.go
@@ -5,11 +5,9 @@ import (
 	"database/sql"
 	"fmt"
 	"io"
-	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/volatiletech/sqlboiler/v4/boil"
-	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 	"learningbay24.de/backend/db"
 	"learningbay24.de/backend/models"
 )
@@ -23,13 +21,9 @@ func GetMaterialFromCourse(db *sql.DB, fileId int) (*models.File, error) {
 	return cm, err
 }
 
-// GetMaterialSliceFromCourse takes a courseID and returns a slice of files associated with it
-func GetMaterialSliceFromCourse(db *sql.DB, courseId int) (models.FileSlice, error) {
-	files, err := models.Files(
-		qm.From(models.TableNames.CourseHasFiles),
-		qm.Where("course_has_files.course_id=?", courseId),
-		qm.And("course_has_files.file_id = file.id"),
-	).All(context.Background(), db)
+// GetAllMaterialsFromCourse takes a courseID and returns a slice of files associated with it
+func GetAllMaterialsFromCourse(db *sql.DB, courseId int) (models.FileSlice, error) {
+	files, err := models.Files(models.CourseHasFileWhere.CourseID.EQ(courseId)).All(context.Background(), db)
 	if err != nil {
 		return nil, err
 	}
@@ -68,7 +62,7 @@ func CreateMaterial(dbHandle *sql.DB, fileName string, uri string, uploaderId, c
 	return nil
 }
 
-// DeleteMaterialFromCourse takes both course-ID and file-ID and deactivates the chosen material
+// DeleteMaterialFromCourse takes both course-ID and file-ID and soft-deletes the chosen material
 func DeleteMaterialFromCourse(db *sql.DB, courseId, fileId int) error {
 	tx, err := db.BeginTx(context.Background(), nil)
 	if err != nil {
@@ -82,39 +76,7 @@ func DeleteMaterialFromCourse(db *sql.DB, courseId, fileId int) error {
 		}
 	}
 
-	// If more than 10 minutes have passed: soft-delete instead of hard-delete
-	curTime := time.Now()
-	diff := curTime.Sub(cm.CreatedAt)
-	if diff.Minutes() > 10 {
-		_, err = cm.Delete(context.Background(), tx, false)
-		if err != nil {
-			if e := tx.Rollback(); e != nil {
-				return fmt.Errorf("fatal: unable to rollback transaction on error: %s; %s", err.Error(), e.Error())
-			}
-		}
-
-		chf, err := models.FindCourseHasFile(context.Background(), tx, courseId, fileId)
-		if err != nil {
-			if e := tx.Rollback(); e != nil {
-				return fmt.Errorf("fatal: unable to rollback transaction on error: %s; %s", err.Error(), e.Error())
-			}
-		}
-
-		_, err = chf.Delete(context.Background(), tx, false)
-		if err != nil {
-			if e := tx.Rollback(); e != nil {
-				return fmt.Errorf("fatal: unable to rollback transaction on error: %s; %s", err.Error(), e.Error())
-			}
-		}
-
-		if e := tx.Commit(); e != nil {
-			return fmt.Errorf("fatal: unable to commit transaction on error: %s; %s", err, e)
-		}
-
-		return nil
-	}
-
-	_, err = cm.Delete(context.Background(), tx, true)
+	_, err = cm.Delete(context.Background(), tx, false)
 	if err != nil {
 		if e := tx.Rollback(); e != nil {
 			return fmt.Errorf("fatal: unable to rollback transaction on error: %s; %s", err.Error(), e.Error())
@@ -128,7 +90,7 @@ func DeleteMaterialFromCourse(db *sql.DB, courseId, fileId int) error {
 		}
 	}
 
-	_, err = chf.Delete(context.Background(), tx, true)
+	_, err = chf.Delete(context.Background(), tx, false)
 	if err != nil {
 		if e := tx.Rollback(); e != nil {
 			return fmt.Errorf("fatal: unable to rollback transaction on error: %s; %s", err.Error(), e.Error())
@@ -150,7 +112,7 @@ func DeleteAllMaterialsFromCourse(db *sql.DB, courseId int, hardDelete bool) err
 		return err
 	}
 
-	materials, err := GetMaterialSliceFromCourse(db, courseId)
+	materials, err := GetAllMaterialsFromCourse(db, courseId)
 	if err != nil {
 		if e := tx.Rollback(); e != nil {
 			return fmt.Errorf("fatal: unable to rollback transaction on error: %s; %s", err, e)
